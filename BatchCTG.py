746b08fbc6d06386a04ff1312125d4795c710ed7
Vermouth7 <thisisabadtry@outlook.com>
2024-08-17 15:34:56 +0800

    del split hidden states

--- BatchCTG.py
+++ BatchCTG.py
@@ -17,7 +17,7 @@ device = torch.device("cuda")
 
 
 def get_condition_output(model,tokenizer,prompts,num_condition,pos):
-    all_hiddens, all_logits, split_hiddens= [], [], []
+    all_hiddens= []
     batches = [prompts[i:i + num_condition] for i in range(0, len(prompts), num_condition)]
     
     for batch_input in tqdm(batches):
@@ -36,17 +36,12 @@ def get_condition_output(model,tokenizer,prompts,num_condition,pos):
         stacked_hidden_states = torch.transpose(stacked_hidden_states, 0, 1)
         
         mean_hidden_states = stacked_hidden_states.mean(dim=0, keepdim=True)
-        mean_logits = logits.mean(dim=0, keepdim=True)
         
 
         all_hiddens.append(mean_hidden_states)
-        all_logits.append(mean_logits)
-        split_hiddens.append(stacked_hidden_states)
     hiddens = torch.cat(all_hiddens, dim=0)
-    logits = torch.cat(all_logits, dim=0)
-    split_hiddens = torch.cat(split_hiddens, dim=0)
 
-    return hiddens, logits,split_hiddens
+    return hiddens
 
 def get_insert_layer(model,tokenizer,task,output):
     val_path='/home/chh/repos/my_ctg/instructions/val/multi_val.jsonl'
@@ -90,59 +85,6 @@ def get_insert_layer(model,tokenizer,task,output):
     
     return layer
 
-def CTG_hs_split(model,tokenizer,task,output):
-    run_results = {}
-
-    start_time = time.time()
-    acc = []
-        
-    # compute_baseline(model,tokenizer,task_name,test_df,eval_models)
-    # best_layer = get_layer(tokenizer=tokenizer,model=model,task_name=task_name,eval_models=eval_models)
-    
-    insert_layer=12
-    print('insert layer: ',insert_layer)
-    run_results[insert_layer]=[]
-    test_data,split,num_condition=process_test_datset(tokenizer,args.task,'/home/chh/repos/my_ctg/instructions/test/multi_lite.jsonl')
-   
-    hiddens,logits,split_hs = get_condition_output(model,tokenizer,split,num_condition,pos=-1)
-    # print(logits.shape)
-    # print(hiddens.shape)
-    
-    
-    control_pipeline = pipeline(
-        "ctg-control", 
-        model=model, 
-        tokenizer=tokenizer, 
-        layers=insert_layer,
-        device=device)
-    
-    
-    batch_size = 8
-    batches_test = [test_data[i:i + batch_size] for i in range(0, len(test_data), batch_size)]
-    batches_split=[split_hs[i:i + batch_size*2] for i in range(0,split_hs.shape[0], batch_size*2)]
-
-    
-    print('Now performing aggregation on layer {}'.format(insert_layer))
-    for index,item in enumerate(tqdm(batches_test, desc="Processing prompts")):
-        inputs=[i['prompt'] for i in item]
-        res = control_pipeline(inputs, activations=batches_split[index],token_pos=[-2,-3],batch_size=batch_size, max_new_tokens=128)
-        res = [item[0]['generated_text'] for item in res]
-        for r,i in zip(res,item):
-            run_results[insert_layer].append({'text': r, 'label1': i['label1'],'label2':i['label2']})
-    
-    with open(output, 'w') as f:
-        json.dump(run_results, f, ensure_ascii=False, indent=2)
-
-    score, layer = compute_metric(output,task)
-    acc.append(score)
-
-    end_time = time.time()
-    print("total run time %.2f" % (end_time - start_time))
-    
-    acc = np.array(acc)
-    acc_mean = np.mean(acc)
-    
-    print("Multi CTG Acc:"+str(acc_mean))
     
     
 def CTG_hs(model,tokenizer,task,output):
